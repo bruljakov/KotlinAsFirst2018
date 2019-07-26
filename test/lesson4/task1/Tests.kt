50f2105d8937b084f417d229ada3bf745b8bcafb
bruljakov <bruljakov@mail.ru>
2019-07-26 14:16:29 +0400

    Урок 2 задачи на IF

--- test/lesson4/task1/Tests.kt
+++ test/lesson4/task1/Tests.kt
@@ -119,6 +119,7 @@ class Tests {
         assertTrue(toMutate === center(toMutate)) { "You should mutate an input list, not create a copy" }
     }
 
+
     @Test
     @Tag("Normal")
     fun times() {
